95e18c2b5e21baa3bc4464644520d70d71e37626
Kenny Nguyen <kenngu90@outlook.com>
2021-08-27 11:27:53 +0200

    .

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -164,6 +164,7 @@ const ImageLink = styled.a`
 
 const Images = styled(GatsbyImage)`
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 2%;
 `
 
 const ProjectColumnTwo = styled.div`
